60399abb2d3c98f994227ab2be71cddc8c4aa558
AsjadJaved <javed.asjad17@gmail.com>
2026-09-19 04:56:47 -0700

    practice html/css/python

---
+++ python/list.py
@@ -0,0 +1,9 @@
+
+haha = [1,2,3,4,5,5,6,7,8,9,10]
+print(haha)
+
+hehe = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five'}
+print(hehe)
+
+abc = {"name": "John", "age": 30, "city": "New York"}
+print(abc)
